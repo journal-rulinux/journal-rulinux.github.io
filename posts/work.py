4504ced993a3e960b316f61bdf9e8af14b05216f
Oleg Pykhalov <go.wigust@gmail.com>
2016-11-01 20:43:39 +0300

    Fix work.py with filename in title

--- posts/work.py
+++ posts/work.py
@@ -5,19 +5,18 @@ import io
 
 for filename in os.listdir('../odt'):
 
-    line = """#+BEGIN_COMMENT
-    .. title: {}
-    .. slug: {}
-    .. date: 2016-11-01 17:39:15 UTC+03:00
-    .. tags: sort
-    .. category:
-    .. link:
-    .. description:
-    .. type: text
-#+END_COMMENT\n""".format(filename, filename)
+    filename_base = filename[:-4]
+
+    line = """.. title: {}
+.. slug: {}
+.. date: 2016-11-01 17:39:15 UTC+03:00
+.. tags: sort
+.. category:
+.. link:
+.. description:
+.. type: text\n""".format(filename_base, filename_base)
 
     filename_meta = filename[:-4] + ".meta"
     with open(filename_meta, "w") as file:
 
         file.write(line)
-
